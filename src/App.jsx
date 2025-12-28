1578c474770f5653d59390f36fcd67538ca3e585
nani191 <yasalapun@gmail.com>
2025-12-28 11:10:26 +0530

    chore: define base frontend folder structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './index.css'
+
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
+      MindForge Frontend Running
+    </h1>
+  );
+}
